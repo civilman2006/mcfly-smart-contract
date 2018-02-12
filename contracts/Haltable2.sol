4fb624f75300f79df1f01fd8f4fd98a6ce53b094
Dmitriy Khizhinskiy <36416400+civilman2006@users.noreply.github.com>
2018-02-12 20:14:08 +0300

    Rename Haltable.sol to Haltable2.sol

---
+++ contracts/Haltable2.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.19;
+
+import './MultiOwners.sol';
+
+contract Haltable is MultiOwners {
+    bool public halted;
+
+    modifier stopInEmergency {
+        require(!halted);
+        _;
+    }
+
+    modifier onlyInEmergency {
+        require(halted);
+        _;
+    }
+
+    // called by the owner on emergency, triggers stopped state
+    function halt() external onlyOwner {
+        halted = true;
+    }
+
+    // called by the owner on end of emergency, returns to normal state
+    function unhalt() external onlyOwner onlyInEmergency {
+        halted = false;
+    }
+
+}
